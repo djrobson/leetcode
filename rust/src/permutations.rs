4635267849a80f1806c3daf5e9a62764fb2ba84b
djrobson <djrobson@users.noreply.github.com>
2025-07-28 14:19:15 -0400

    rust permutations solved

--- rust/src/permutations.rs
+++ rust/src/permutations.rs
@@ -3,7 +3,23 @@
 
 impl Solution {
     pub fn permute(nums: Vec<i32>) -> Vec<Vec<i32>> {
-        todo!("Fill in body")
+        let mut permutations = Vec::new();
+        fn descend( pin_offset: usize, input: &mut Vec<i32>, output: &mut Vec<Vec<i32>>){
+            if pin_offset == input.len() {
+                // we've pinned and swapped all the way to the end, save a clone
+                output.push(input.clone())
+            }
+            for i in pin_offset..input.len() {
+                // for each spot we can swap with, swap it and descend, then unswap it
+                input.swap(pin_offset, i);
+                descend(pin_offset+1, input, output);
+                input.swap(pin_offset, i);
+            }
+        }
+
+        let mut input= nums;
+        descend(0, &mut input, &mut permutations);
+        permutations
     }
 }
 
@@ -18,7 +34,7 @@ mod tests {
     use rstest::rstest;
 
     #[rstest]
-    #[case(vec![1,2,3], vec![vec![1,2,3],vec![1,3,2],vec![2,1,3],vec![2,3,1],vec![3,1,2],vec![3,2,1]])]
+    #[case(vec![1,2,3], vec![vec![1,2,3],vec![1,3,2],vec![2,1,3],vec![2,3,1],vec![3,2,1],vec![3,1,2]])]
     #[case(vec![0,1], vec![vec![0,1],vec![1,0]])]
     #[case(vec![1], vec![vec![1]])]
     fn case(#[case] nums: Vec<i32>, #[case] expected: Vec<Vec<i32>>) {
